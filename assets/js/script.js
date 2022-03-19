160a724419b6afc24739fb17fe0167e2466fe973
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-03-19 17:06:07 -0400

    Updated initial starting page
    Added additional html and css for the initial starting page

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+//assignment 4: {
+//     "question": "Commonly used data types do NOT include:",
+//     "choices": ["Strings", "Booleans", "Alerts", "Numbers"],
+//     "correctChoice": "Alerts"
+// },
+// {
+//     "question": "The condition in an if/else statement is enclosed in:",
+//     "choices": ["Quotes", "Parenthesis", "Curly Brackets", "Square Brackets"],
+//     "correctChoice": "Parenthesis"
+// },
+// {
+//     "question": "Arrays in JavaScript can be used to store:",
+//     "choices": ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+//     "correctChoice": "All of the above"
+// },
+// {
+//     "question": "String values must be enclosed within __ when being assigned to variables",
+//     "choices": ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+//     "correctChoice": "Quotes"
+// },
+// {
+//     "question": "A very useful tool used during development and debugging for printing content to the debugger is:",
+//     "choices": ["JavaScript", "Terminal Bash", "for loops", "console.log"],
+//     "correctChoice": "console.log"
+// }

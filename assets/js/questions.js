b01249782cc3a79b6dd4952077bc534290c12509
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-03-20 14:47:45 -0400

    Added new files and updated old files
    Updated file structures and added new reset.css styles for when returning to home page as well as questions.js for questions to be accessed by js. Adjusted CSS to give color to the page

---
+++ assets/js/questions.js
@@ -0,0 +1,41 @@
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+    answer: "Alerts",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+    answer: "Parentheses",
+  },
+  {
+    title:
+      "Which built-in method removes the last element from an array and returns that element?",
+    choices: ["Last()", "Get()", "Pop()", "None of the Above"],
+    answer: "Pop()",
+  },
+  {
+    title:
+      "Which built-in method returns the calling string value converted to lower case?",
+    choices: [
+      "toLowerCase()",
+      "toLower()",
+      "changeCase(case)",
+      "None of the Above",
+    ],
+    answer: "toLowerCase()",
+  },
+  {
+    title:
+      "Which of the following function of Number object returns the number's value",
+    choices: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
+    answer: "valueOf()",
+  },
+  {
+    title:
+      "Which of the following function of Array object joins all elements of an array into a string?",
+    choices: ["concat()", "join()", "pop()", "map()"],
+    answer: "join()",
+  },
+];
